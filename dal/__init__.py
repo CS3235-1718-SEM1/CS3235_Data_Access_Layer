455474a391631d3c5bab38ce3a3ededc75a6ab6b
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-10-11 21:38:03 +0800

    Created a student model and endpoints that interact with the DB

--- dal/__init__.py
+++ dal/__init__.py
@@ -1,9 +1,18 @@
 """
 Data Access Layer abstracts database accesses by exposing a RESTful API
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# TODO: extract this to config file, one for development and another for production
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cs3235'
+
+db = SQLAlchemy(app)
+
+# Import models only after db is defined so that the DB schema can be properly setup
+from . import models
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,5 +25,31 @@ def index():
         if request.json is not None:
             return jsonify(request.json)
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+
+@app.route('/students', methods=['GET'])
+def get_student():
+    # TODO: find a better way to serialize data
+    return jsonify({
+        'result': [{
+            'id': student.id,
+            'matric_no': student.matric_no,
+            'name': student.name,
+            'email': student.email
+                   } for student in models.Student.query.all()]
+    })
+
+
+@app.route('/students', methods=['POST'])
+def create_student():
+    post_data = request.json
+    is_successful = True
+    try:
+        new_student = models.Student(matric_no=post_data['matric_no'], name=post_data['name'], email=post_data['email'])
+        db.session.add(new_student)
+        db.session.commit()
+    except Exception:
+        is_successful = False
+
+    return jsonify({
+        'is_successful': is_successful
+    })
