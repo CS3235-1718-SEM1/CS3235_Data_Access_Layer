631d8c400c4c140cbcfeef5fde2bcadf115c922e
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-10-17 21:10:14 +0800

    Adding an empty function for populating rooms later

--- dal/models/db_population.py
+++ dal/models/db_population.py
@@ -7,11 +7,17 @@ import json
 from .. import db
 from .module import Module
 
-with open('./dal/models/module_list.json') as f:
-    modules_dict = json.load(f)
 
-for code in modules_dict.keys():
-    new_module = Module(code=code)
-    db.session.add(new_module)
+def populate_module_list():
+    with open('./dal/models/module_list.json') as f:
+        modules_dict = json.load(f)
 
-db.session.commit()
+    for code in modules_dict.keys():
+        new_module = Module(code=code)
+        db.session.add(new_module)
+
+    db.session.commit()
+
+
+def populate_rooms():
+    pass
